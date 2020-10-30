0e01654de640888b5ac8594bbe976ca522415534
十八子李 <94770222@qq.com>
2020-10-30 14:23:00 +0800

    [note] vue-router add note

--- vue-router-mini/src/vue-router/components/view.js
+++ vue-router-mini/src/vue-router/components/view.js
@@ -4,15 +4,18 @@ export default {
 
 		let parent = this.$parent;
 
+		// 嵌套路由 逐层查找父级，看看当前视图嵌套的深度
 		let depth = 0;
 		while (parent) {
 			const vnodeData = parent.$vnode ? parent.$vnode.data : {};
 			if (vnodeData.routerView) {
+				// 发现一个视图 深度+1
 				depth++;
 			}
 			parent = parent.$parent;
 		}
 
+		// 取出 router 配置对应的组件并渲染
 		const matched = this.$router.matched;
 		const route = matched[depth];
 		const component = route ? route.component : null;
--- vue-router-mini/src/vue-router/index.js
+++ vue-router-mini/src/vue-router/index.js
@@ -9,20 +9,17 @@ class VueRouter {
 		this.routeMap = {};
 
 		this.current = window.location.hash.slice(1) || '/';
+
+		// 数据必须是响应式的，这样change的时候才会更新视图
+		// this.matched = [];
 		Vue.util.defineReactive(this, 'matched', []);
 
 		this.match();
 	}
 
 	init() {
-		// this.createRouterMap();
+		// 监听 hash 变化
 		this.bindEvents();
-	}
-
-	createRouterMap() {
-		this.$options.routes.forEach((route) => {
-			this.routeMap[route.path] = route;
-		});
 	}
 
 	bindEvents() {
@@ -36,17 +33,21 @@ class VueRouter {
 	}
 
 	match(routes) {
+		// 根据 current 在 routes 中查询相匹配的 route
 		routes = routes || this.$options.routes;
 
 		for (const route of routes) {
 			if (route.path === '/' && this.current === '/') {
+				// 根路由直接返回
 				this.matched.push(route);
 				return;
 			}
 			if (route.path !== '/' && ~this.current.indexOf(route.path)) {
+				//  [ { path: '/about', ...} ]
 				this.matched.push(route);
 				const children = route.children;
 				if (children && children.length) {
+					//  [ { path: '/about', ...}, { path: '/about/info' } ]
 					this.match(children);
 				}
 				return;
@@ -55,19 +56,23 @@ class VueRouter {
 	}
 }
 
+// Vue 插件必须实现 install 方法
 VueRouter.install = function(_Vue) {
 	Vue = _Vue;
 
+	// 全局混入
 	Vue.mixin({
 		beforeCreate() {
 			const router = this.$options.router;
 			if (router) {
+				// 挂载 router 并初始化
 				Vue.prototype.$router = router;
 				router.init();
 			}
 		},
 	});
 
+	// 注册全局组件
 	Vue.component('router-link', Link);
 	Vue.component('router-view', View);
 };
